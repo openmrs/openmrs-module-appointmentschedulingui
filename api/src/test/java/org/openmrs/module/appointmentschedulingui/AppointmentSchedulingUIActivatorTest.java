e787b1bf15253aee0c1bd9b37f29106fd7af1c0a
mogoodrich <mgoodrich@pih.org>
2014-01-10 15:24:33 -0500

    added activator test, since bamboo evidently expects there to be at least one test

---
+++ api/src/test/java/org/openmrs/module/appointmentschedulingui/AppointmentSchedulingUIActivatorTest.java
@@ -0,0 +1,17 @@
+package org.openmrs.module.appointmentschedulingui;
+
+import org.junit.Test;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+
+public class AppointmentSchedulingUIActivatorTest extends BaseModuleContextSensitiveTest {
+
+    @Test
+    public void testStartup() throws Exception {
+        AppointmentSchedulingUIActivator activator = new AppointmentSchedulingUIActivator();
+        activator.willRefreshContext();
+        activator.contextRefreshed();
+        activator.willStart();
+        activator.started();
+    }
+}
+
