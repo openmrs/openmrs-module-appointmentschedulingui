2e4f3681ea77177d59861131d1d40727e532e3fa
Pamela Canchanya <pmori@pmori.local>
2014-02-25 18:13:33 -0300

    UHM-855 Remove attributes not longer used

--- omod/src/main/java/org/openmrs/module/appointmentschedulingui/page/controller/DailyScheduledAppointmentsPageController.java
+++ omod/src/main/java/org/openmrs/module/appointmentschedulingui/page/controller/DailyScheduledAppointmentsPageController.java
@@ -1,15 +1,11 @@
 package org.openmrs.module.appointmentschedulingui.page.controller;
 
 import org.openmrs.Location;
-import org.openmrs.module.appointmentscheduling.ScheduledAppointmentBlock;
 import org.openmrs.module.appointmentscheduling.api.AppointmentService;
 import org.openmrs.module.appui.UiSessionContext;
 import org.openmrs.module.emrapi.EmrApiProperties;
 import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.page.PageModel;
-
-import java.util.Date;
-import java.util.List;
 
 public class DailyScheduledAppointmentsPageController {
 
@@ -18,9 +14,6 @@ public class DailyScheduledAppointmentsPageController {
                     UiSessionContext uiSessionContext){
 
        Location location = uiSessionContext.getSessionLocation();
-       List<ScheduledAppointmentBlock> scheduledAppointmentBlocks = appointmentService.getDailyAppointmentBlocks(location, new Date());
-       pageModel.addAttribute("scheduledAppointmentBlocks", scheduledAppointmentBlocks);
-       pageModel.addAttribute("primaryPatientIdentifierType", emrApiProperties.getPrimaryIdentifierType() );
-       pageModel.addAttribute("dossierPatientIdentifierType", emrApiProperties.getDossierIdentifierType() );
+       pageModel.addAttribute("currentLocationUuid", location.getUuid() );
     }
 }
