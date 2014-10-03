a92515dce72f787ff7355f5add335f9f5d42eb86
mogoodrich <mgoodrich@pih.org>
2014-10-03 12:13:27 -0400

    make appointment widget date sort null safe

--- omod/src/main/webapp/resources/scripts/controllers/patientAppointmentsController.js
+++ omod/src/main/webapp/resources/scripts/controllers/patientAppointmentsController.js
@@ -83,13 +83,15 @@ angular.module('appointmentscheduling')
                     result['displayStatus'] = emr.message("appointmentschedulingui.scheduleAppointment.status.type." + result["status"].type.toLowerCase());
                 });
 
-                results.sort(function(a, b) {
-                    if (a.timeSlot.appointmentBlock.startDate > b.timeSlot.appointmentBlock.startDate) {
-                        return 1;
-                    } else if (a.timeSlot.appointmentBlock.startDate < b.timeSlot.appointmentBlock.startDate) {
-                        return -1;
-                    } return 0;
-                });
+                if (results) {
+                    results.sort(function(a, b) {
+                        if (a.timeSlot.appointmentBlock.startDate > b.timeSlot.appointmentBlock.startDate) {
+                            return 1;
+                        } else if (a.timeSlot.appointmentBlock.startDate < b.timeSlot.appointmentBlock.startDate) {
+                            return -1;
+                        } return 0;
+                    });
+                }
 
                 initializeMessagesAfterSearch(results);
                 $scope.pagingOptions.currentPage = 1;
