4a6e6c9430bd439030649761b17f3d8d6c6917c0
mogoodrich <mgoodrich@pih.org>
2014-11-04 17:17:24 -0500

    renamed appointments tab application

--- omod/src/main/webapp/resources/scripts/controllers/appointmentsTabController.js
+++ omod/src/main/webapp/resources/scripts/controllers/appointmentsTabController.js
@@ -6,7 +6,7 @@ angular.module('appointmentscheduling')
 
                 // TODO kind of hacky because it depends on the format/implementation of the tab to always be this href format
                 // don't load the appointments and appointment requests until the appt tab is select (we are running into a angular error I can solve if we try to pre-load these)
-                jq("a[href='#org.openmrs.module.appointmentschedulingui.tab']").click(function() {
+                jq("a[href='#appointmentschedulingui.tab']").click(function() {
                     $scope.$broadcast('appointmentRequests.loadAppointmentRequests');
                     $scope.$broadcast('appointmentRequests.loadAppointments');
                 })
