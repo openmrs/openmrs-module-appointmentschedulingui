8095498bf52726f740fe64212dacd279228d53cd
Ian <52504170+ibacher@users.noreply.github.com>
2021-02-26 15:33:19 -0500

    Update omod/src/main/webapp/resources/scripts/controllers/requestAppointmentController.js

--- omod/src/main/webapp/resources/scripts/controllers/requestAppointmentController.js
+++ omod/src/main/webapp/resources/scripts/controllers/requestAppointmentController.js
@@ -101,7 +101,7 @@ angular.module('appointmentscheduling.requestAppointment')
 
             AppointmentService.saveAppointmentRequest(appointmentRequestToSave).then(function() {
                 // success callback
-            	emr.successAlert("requestAppointment saved successfully");
+                emr.successAlert("requestAppointment saved successfully");
                 redirectToReturnPage();
             }).
             catch(function () {
