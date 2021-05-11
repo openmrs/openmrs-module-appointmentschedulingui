719cd6e055a3e3fa500569315ea1d35bfa9a5a7c
CrooksDC <dylanccrooks@gmail.com>
2021-05-10 21:48:52 -0400

    EMPT 21 patch

--- omod/src/main/java/org/openmrs/module/appointmentschedulingui/page/controller/AppointmentRequestsPageController.java
+++ omod/src/main/java/org/openmrs/module/appointmentschedulingui/page/controller/AppointmentRequestsPageController.java
@@ -1,5 +1,6 @@
 package org.openmrs.module.appointmentschedulingui.page.controller;
 
+import org.openmrs.api.APIAuthenticationException;
 import org.openmrs.module.appointmentschedulingui.AppointmentSchedulingUIConstants;
 import org.openmrs.module.appui.UiSessionContext;
 import org.openmrs.ui.framework.page.PageModel;
@@ -7,6 +8,11 @@ import org.openmrs.ui.framework.page.PageModel;
 public class AppointmentRequestsPageController {
 
     public Object controller(PageModel model, UiSessionContext uiSessionContext) {
+		uiSessionContext.requireAuthentication();
+		if (!uiSessionContext.getCurrentUser().hasPrivilege("Task: appointmentschedulingui.requestAppointments")
+		        && (!uiSessionContext.getCurrentUser().isSuperUser())) {
+			throw new APIAuthenticationException();
+		}
         model.addAttribute("canBook", uiSessionContext.getCurrentUser().hasPrivilege(AppointmentSchedulingUIConstants.PRIVILEGE_BOOK_APPOINTMENTS));
         return null;
     }
