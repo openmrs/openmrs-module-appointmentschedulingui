19518969a1b4fdb2b0666ce7f604eb9f1523ccf6
CrooksDC <48846327+CrooksDC@users.noreply.github.com>
2021-05-11 14:10:36 -0400

    RA-1875: EMPT 21 in AppointmentRequestsPageController.java (#24)
    * EMPT 21 patch
    
    * Added the exception description

--- omod/src/main/java/org/openmrs/module/appointmentschedulingui/page/controller/AppointmentRequestsPageController.java
+++ omod/src/main/java/org/openmrs/module/appointmentschedulingui/page/controller/AppointmentRequestsPageController.java
@@ -1,5 +1,6 @@
 package org.openmrs.module.appointmentschedulingui.page.controller;
 
+import org.openmrs.api.APIAuthenticationException;
 import org.openmrs.module.appointmentschedulingui.AppointmentSchedulingUIConstants;
 import org.openmrs.module.appui.UiSessionContext;
 import org.openmrs.ui.framework.page.PageModel;
@@ -7,6 +8,12 @@ import org.openmrs.ui.framework.page.PageModel;
 public class AppointmentRequestsPageController {
 
     public Object controller(PageModel model, UiSessionContext uiSessionContext) {
+		uiSessionContext.requireAuthentication();
+		if (!uiSessionContext.getCurrentUser().hasPrivilege("Task: appointmentschedulingui.requestAppointments")
+		        && (!uiSessionContext.getCurrentUser().isSuperUser())) {
+			throw new APIAuthenticationException(
+			        "Access to appointment requests page denied due to insufficient privileges");
+		}
         model.addAttribute("canBook", uiSessionContext.getCurrentUser().hasPrivilege(AppointmentSchedulingUIConstants.PRIVILEGE_BOOK_APPOINTMENTS));
         return null;
     }
