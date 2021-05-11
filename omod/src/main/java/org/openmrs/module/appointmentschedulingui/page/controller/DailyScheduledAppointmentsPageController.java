e8a3d81aec3acab8f0b95301f40be6a9801346e1
CrooksDC <48846327+CrooksDC@users.noreply.github.com>
2021-05-11 15:53:47 -0400

    RA-1875: EMPT 22 in DailyScheduledAppointmentsPageController.java (#23)
    * EMPT 22 patch
    
    * Update DailyScheduledAppointmentsPageController.java
    
    * Added the exception description

--- omod/src/main/java/org/openmrs/module/appointmentschedulingui/page/controller/DailyScheduledAppointmentsPageController.java
+++ omod/src/main/java/org/openmrs/module/appointmentschedulingui/page/controller/DailyScheduledAppointmentsPageController.java
@@ -2,6 +2,7 @@ package org.openmrs.module.appointmentschedulingui.page.controller;
 
 import org.openmrs.Location;
 import org.openmrs.LocationTag;
+import org.openmrs.api.APIAuthenticationException;
 import org.openmrs.module.appointmentschedulingui.AppointmentSchedulingUIProperties;
 import org.openmrs.module.appui.UiSessionContext;
 import org.openmrs.module.emrapi.EmrApiConstants;
@@ -13,7 +14,12 @@ public class DailyScheduledAppointmentsPageController {
     public void get(PageModel pageModel,
                     @SpringBean("appointmentSchedulingUIProperties")AppointmentSchedulingUIProperties properties,
                     UiSessionContext uiSessionContext){
-
+		
+		uiSessionContext.requireAuthentication();
+		if (!uiSessionContext.getCurrentUser().hasPrivilege("App: appointmentschedulingui.viewAppointments")
+		        && (!uiSessionContext.getCurrentUser().isSuperUser())) {
+			throw new APIAuthenticationException("Access to daily scheduled appointments page denied due to insufficient privileges");
+		}
        Location location = uiSessionContext.getSessionLocation();
        LocationTag supportsAppointmentsTag =  properties.getSupportsAppointmentsTag();
        pageModel.addAttribute("supportsAppointmentsTagUuid",
